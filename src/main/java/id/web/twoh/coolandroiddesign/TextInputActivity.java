d6ac8b9049db8d363ceceee1184be377af27341b
twoh <hherdinaufal@gmail.com>
2015-07-22 17:20:33 +0700

    add Android Toolbartutorial

--- src/main/java/id/web/twoh/coolandroiddesign/TextInputActivity.java
+++ src/main/java/id/web/twoh/coolandroiddesign/TextInputActivity.java
@@ -2,6 +2,8 @@ package id.web.twoh.coolandroiddesign;
 
 import android.os.Bundle;
 import android.support.v7.app.AppCompatActivity;
+import android.support.v7.widget.Toolbar;
+import android.view.Menu;
 
 /**
  * Created by Hafizh Herdi on 7/9/2015.
@@ -12,5 +14,34 @@ public class TextInputActivity extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_textinput);
+
+        // findview toolbar
+        Toolbar toolbar = (Toolbar) findViewById(R.id.toolbar);
+
+        // set toolbar ke dalam support action bar
+        setSupportActionBar(toolbar);
+
+        // enable home button untuk navigasi
+        getSupportActionBar().setDisplayHomeAsUpEnabled(true);
+
+        // mengeset icon untuk home button Toolbar
+        getSupportActionBar().setHomeAsUpIndicator(R.drawable.ic_menu);
+
+        // mengeset title/nama aplikasi
+        getSupportActionBar().setTitle("TWOH's Engineering");
+
+        // mengeset subtitle
+        getSupportActionBar().setSubtitle("Tutorial Material Design");
+
+        // set logo toolbar
+        getSupportActionBar().setDisplayUseLogoEnabled(true);
+        getSupportActionBar().setLogo(R.mipmap.ic_launcher);
+    }
+
+    @Override
+    public boolean onCreateOptionsMenu(Menu menu) {
+        // Inflate the menu; this adds items to the action bar if it is present.
+        getMenuInflater().inflate(R.menu.menu_main, menu);
+        return true;
     }
 }
