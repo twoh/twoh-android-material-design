79bc3b19e5146c5cdacf99dfa443a954b51402ea
twoh <hherdinaufal@gmail.com>
2015-07-24 10:30:28 +0700

    updatedAndroid Navigation View

--- src/main/java/id/web/twoh/coolandroiddesign/NavigationViewActivity.java
+++ src/main/java/id/web/twoh/coolandroiddesign/NavigationViewActivity.java
@@ -1,11 +1,13 @@
 package id.web.twoh.coolandroiddesign;
 
 import android.os.Bundle;
+import android.support.design.widget.NavigationView;
 import android.support.v4.view.GravityCompat;
 import android.support.v4.widget.DrawerLayout;
 import android.support.v7.app.AppCompatActivity;
 import android.support.v7.widget.Toolbar;
 import android.view.MenuItem;
+import android.widget.Toast;
 
 /**
  * Created by Hafizh Herdi on 7/23/2015.
@@ -14,6 +16,7 @@ public class NavigationViewActivity extends AppCompatActivity {
 
     private Toolbar toolbar;
     private DrawerLayout drawerLayout;
+    private NavigationView navView;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -21,6 +24,33 @@ public class NavigationViewActivity extends AppCompatActivity {
         setContentView(R.layout.activity_nav_view);
         drawerLayout = (DrawerLayout) findViewById(R.id.drawer_layout);
         setupToolbar();
+        navView = (NavigationView) findViewById(R.id.navigation);
+        navView.setNavigationItemSelectedListener(new NavigationView.OnNavigationItemSelectedListener() {
+            @Override
+            public boolean onNavigationItemSelected(MenuItem menuItem) {
+                if(menuItem.isChecked())
+                    menuItem.setChecked(false);
+                else
+                    menuItem.setChecked(true);
+
+                drawerLayout.closeDrawers();
+
+                switch (menuItem.getItemId()){
+
+                    case R.id.nav_home:
+                        Toast.makeText(NavigationViewActivity.this, "Home clicked", Toast.LENGTH_SHORT).show();
+                        return true;
+                    case R.id.nav_my_profile:
+                        Toast.makeText(NavigationViewActivity.this, "My profile clicked", Toast.LENGTH_SHORT).show();
+                        return true;
+                    case R.id.nav_settings:
+                        Toast.makeText(NavigationViewActivity.this, "Settings clicked", Toast.LENGTH_SHORT).show();
+                        return true;
+
+                }
+                return true;
+            }
+        });
     }
 
     @Override
