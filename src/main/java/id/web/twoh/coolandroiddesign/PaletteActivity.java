7134c4f49161093f65c3b585f3c3af0464391d95
twoh <hherdinaufal@gmail.com>
2016-09-26 00:10:29 +0700

    add palette

--- src/main/java/id/web/twoh/coolandroiddesign/PaletteActivity.java
+++ src/main/java/id/web/twoh/coolandroiddesign/PaletteActivity.java
@@ -1,7 +1,70 @@
 package id.web.twoh.coolandroiddesign;
 
+import android.graphics.Bitmap;
+import android.graphics.BitmapFactory;
+import android.os.Bundle;
+import android.support.v7.app.AppCompatActivity;
+import android.support.v7.graphics.Palette;
+import android.widget.TextView;
+
 /**
  * Created by Herdi_WORK on 18.09.16.
  */
-public class PaletteActivity {
+public class PaletteActivity extends AppCompatActivity{
+
+    private TextView vibrantView;
+    private TextView vibrantLightView;
+    private TextView vibrantDarkView;
+    private TextView mutedView;
+    private TextView mutedLightView;
+    private TextView mutedDarkView;
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_pallete);
+
+        initViews();
+        paintTextBackground();
+
+    }
+
+
+    private void initViews() {
+        vibrantView = (TextView) findViewById(R.id.tvVibrant);
+        vibrantLightView = (TextView) findViewById(R.id.tvVibrantLight);
+        vibrantDarkView = (TextView) findViewById(R.id.tvVibrantDark);
+        mutedView = (TextView) findViewById(R.id.tvMuted);
+        mutedLightView = (TextView) findViewById(R.id.tvMutedLight);
+        mutedDarkView = (TextView) findViewById(R.id.tvMutedDark);
+
+    }
+
+    private void paintTextBackground() {
+
+        Bitmap bitmap = BitmapFactory.decodeResource(getResources(), R.drawable.draw_pallete);
+
+        Palette.from(bitmap).generate(new Palette.PaletteAsyncListener() {
+            @Override
+            public void onGenerated(Palette palette) {
+                //work with the palette here
+                int defaultValue = 0x000000;
+                int vibrant = palette.getVibrantColor(defaultValue);
+                int vibrantLight = palette.getLightVibrantColor(defaultValue);
+                int vibrantDark = palette.getDarkVibrantColor(defaultValue);
+                int muted = palette.getMutedColor(defaultValue);
+                int mutedLight = palette.getLightMutedColor(defaultValue);
+                int mutedDark = palette.getDarkMutedColor(defaultValue);
+
+                vibrantView.setBackgroundColor(vibrant);
+                vibrantLightView.setBackgroundColor(vibrantLight);
+                vibrantDarkView.setBackgroundColor(vibrantDark);
+                mutedView.setBackgroundColor(muted);
+                mutedLightView.setBackgroundColor(mutedLight);
+                mutedDarkView.setBackgroundColor(mutedDark);
+            }
+        });
+
+
+    }
 }
